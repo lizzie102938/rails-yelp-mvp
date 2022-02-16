e9f15891d9e40b1c5e696e25a2d274f0b7bdc2bc
lizzie lerwill <lizzielerwill@hotmail.com>
2022-02-16 16:00:01 +0100

    half way maybe

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,41 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-    resources :restaurants
+  resources :restaurants, only: [:index, :new, :create, :show] do
+    resources :reviews, only: [:new, :create]
+  end
 end
+
+# A visitor can add a new review to a restaurant
+# GET "restaurants/38/reviews/new" - new
+# POST "restaurants/38/reviews" - create
+
+# Rails.application.routes.draw do
+#   resources :restaurants do
+#     resources :reviews, only: [:new, :create]
+#     # /restaurants/:restaurant_id/reviews/new
+
+#     # Beyond normal CRUD routes
+
+#     # # Collection routes — these are like a "custom index"
+#     # collection do
+#     #   # makes a route like /restaurants/CUSTOM_THING
+#     #   get "top"
+#     #   # get "low_rated"
+#     #   # get "berlin"
+#     # end
+
+#     # # Member routes — these are like a "custom show"
+#     # member do
+#     #   # make a route like /restaurants/:id/CUSTOM_THING
+#     #   get "chef"
+#     # end
+#   end
+
+#   resources :reviews, only: [:destroy]
+#   # /reviews/5
+
+#   # get "/restaurants/:restaurant_id/reviews/new", to: "reviews#new"
+
+#   # get "/restaurants/top", to: "restaurants#top"
+#   # get "/restaurants/:id/chef", to: "restaurants#chef"
+# end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts "Cleaning database..."
+Restaurant.destroy_all
+
+puts "Creating restaurants..."
+homeslice = { name: "Homeslice", address: "374-378 Old St, London EC1V 9LT", phone_number: "020 3151 1121", category: "chinese" }
+brat = { name: "Brat", address: "4 Redchurch St, London E1 6JL", phone_number: "020 3883 3525", category: "italian" }
+clove_club = { name: "Clove Club", address: "Shoreditch Town Hall, 380 Old St, London EC1V 9LT", phone_number: "020 7729 6496", category: "japanese" }
+silk_road = { name: "Silk Road", address: "49 Camberwell Church St, London SE5 8TR", phone_number: "020 7703 4832", category: "french" }
+leroy = { name: "Leroy", address: "18 Phipp St, London EC2A 4NU", phone_number: "020 7739 4443", category: "italian" }
+
+[homeslice, brat, clove_club, silk_road, leroy].each do |attributes|
+  restaurant = Restaurant.create!(attributes)
+  puts "Created #{restaurant.name}"
+end
+puts "Finished!"
